af257528273639b06a9e21999b0a59a902261a64
korn <kornjiradej@gmail.com>
2019-06-19 14:54:29 +0700

    Create app, import korn.pic

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <NavbarComponent></NavbarComponent>
+    <!-- <img src="./assets/logo.png"> -->
+    <img src="./assets/korn.jpg">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import NavbarComponent from "@/components/NavbarComponent";
+
+export default {
+  name: "App",
+  components: {
+    NavbarComponent
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>{{massage}}</h1>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            massage:"Korn jiradej right here !"
+        }
+    }
+}
+</script>
