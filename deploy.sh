d5b627f3165c6701aed229d0ed141318046986fe
korn <kornjiradej@gmail.com>
2019-06-19 17:34:27 +0700

    .

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,25 @@
-# deploy.sh
-
 #!/usr/bin/env sh
 
 # abort on errors
 set -e
 
 # build
-echo Linting..
-npm run lint
-echo Building. this may take a minute...
 npm run build
 
 # navigate into the build output directory
 cd dist
 
 # if you are deploying to a custom domain
-# echo 'example.com' > CNAME
+# echo 'www.example.com' > CNAME
 
-echo Deploying..
 git init
 git add -A
 git commit -m 'deploy'
 
-# deploy
-# git push -f git@github.com:jiradejP/resume.git master
-# git push -f git@github.com:jiradejP/jiradejp.github.io.git master
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:jiradejP/resume.git master:gh-pages
 
 cd -
