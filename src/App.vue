0f54d91f51b3b2dc46e4194e9a475d6e3b641a88
korn <kornjiradej@gmail.com>
2019-06-20 11:52:29 +0700

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <div id="app">
-    <NavbarComponent></NavbarComponent>
-    <img src="./assets/logo.png">
-    <!-- <img src="./assets/korn.jpg"> -->
-    <router-view/>
-  </div>
+  <b-container>
+    <!-- Content here -->
+    <b-container class="bv-example-row">
+      <b-img center v-bind="picture2" :src="require('../src/assets/korn.jpg')"></b-img>
+      <b-row class="justify-content-md-center">
+        
+        <b-col cols="12" md="auto">
+          <!-- <b-img center  src="./assets/korn.jpg" rounded="circle" alt="Circle image"></b-img> -->
+          <!-- <img center src="./assets/logo.png"> -->
+          
+          <b-img center v-bind="picture1" :src="require('../src/assets/logo.png')" ></b-img>
+          <!-- <b-img center src="https://picsum.photos/125/125/?image=58" alt="Center image"></b-img> -->
+          <router-view/>
+        </b-col>
+        
+      </b-row>
+
+    </b-container>
+    
+  </b-container>
 </template>
 
 <script>
-import NavbarComponent from "@/components/NavbarComponent";
-
 export default {
   name: "App",
-  components: {
-    NavbarComponent
+  data(){
+    return{
+      picture1:{width: '100%', height: '100%'},
+      picture2:{ width: '100%' }
+    }
   }
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NavbarComponent></NavbarComponent>
+  </div>
+</template>
+
+<script>
+import NavbarComponent from "@/components/NavbarComponent";
+
+export default{
+  name: 'Nav',
+  components:{
+      NavbarComponent
+  }
+}
+
+</script>
+
